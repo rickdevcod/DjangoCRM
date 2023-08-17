1210b3afac03fadf8ad1a81263fc20ead9aeaaa5
rickDevcod <tribulus22214@gmail.com>
2023-08-16 22:18:33 -0300

    registration

--- website/views.py
+++ website/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+def home(request):
+	# Verificar se esta fazendo loguin
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		# Autenticação
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, "Sucesso ao se conectar")
+			return redirect('home')
+		else:
+			messages.success(request, "Usuário ou Senha incorretos, Tente Novamente!")
+			return redirect('home')
+	else:
+		return render(request, 'home.html', {})
+
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, "Você foi desconectado")
+	return redirect('home')
+
+def register_user(request):
+	return render(request, 'register.html', {})
